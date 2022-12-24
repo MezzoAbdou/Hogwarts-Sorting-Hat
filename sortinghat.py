fa8f64abe6ab5bd2c2320f7c106c552824cf3091
Lindsay Abdou <abdoulindsay@gmail.com>
2022-12-23 20:46:52 -0800

    need to come up with question conditions

--- sortinghat.py
+++ sortinghat.py
@@ -3,6 +3,7 @@ print("Welcome to Hogwarts!")
 question = input("Would you like to know which Hogwarts house you belong to? ")
 
 if question.lower() != "yes":
+    print("Well you are not magical anyway")
     quit()
     
 print("Let's get the sorting Hat!")    
@@ -10,3 +11,5 @@ print("Let's get the sorting Hat!")
 condition = input("Are you nice? y or n ")
 if condition.lower() == "y":
  print("Hufflepuff!")
+if condition.lower() == "n": 
+ print("Slytherin") 
