06d0fce32ab35c51ff4182d52e50c6331569c8f0
Lindsay Abdou <abdoulindsay@gmail.com>
2023-01-13 11:16:51 -0800

    changed ide

---
+++ HogwartsSortingHat.py
@@ -0,0 +1,29 @@
+def game():
+
+    #welcome statement
+    print("Welcome to Hogwarts!")
+    
+    
+
+    
+    #User input to enter process
+    question = input("Would you like to know which Hogwarts house you belong to? Type yes or no... ")
+
+    #compare answers, and continue process
+    if question.lower() == "no":
+        print("Well you are not magical anyway")
+        quit()
+    if question.lower() != ("no" or "yes") :
+        print("I said yes or no!")
+    
+    
+        print("Let's get the sorting Hat!")    
+
+    #question 2
+    condition = input("Are you nice? y or n ") 
+    if condition.lower() == "y":
+        print("Hufflepuff!")
+    if condition.lower() == "n": 
+        print("Slytherin")
+
+game()        
