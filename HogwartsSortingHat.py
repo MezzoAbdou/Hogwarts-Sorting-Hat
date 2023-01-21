a1f76fa6a775da4fbfcd44a7d09267a06dc44b47
Lindsay Abdou <abdoulindsay@gmail.com>
2023-01-20 20:27:18 -0800

    Working on the functionality

--- HogwartsSortingHat.py
+++ HogwartsSortingHat.py
@@ -1,4 +1,6 @@
 def game():
+    def response1():
+        print("Let's get the sorting Hat!")   
 
     #welcome statement
     print("Welcome to Hogwarts!")
@@ -9,21 +11,22 @@ def game():
     #User input to enter process
     question = input("Would you like to know which Hogwarts house you belong to? Type yes or no... ")
 
-    #compare answers, and continue process
+##    #compare answers, and continue process
+    if question.lower() == "yes":
+           response1()
     if question.lower() == "no":
-        print("Well you are not magical anyway")
-        quit()
+          print("Well you are not magical anyway")
+          quit()
     if question.lower() != ("no" or "yes") :
-        print("I said yes or no!")
-    
-    
-        print("Let's get the sorting Hat!")    
-
-    #question 2
-    condition = input("Are you nice? y or n ") 
-    if condition.lower() == "y":
-        print("Hufflepuff!")
-    if condition.lower() == "n": 
-        print("Slytherin")
-
-game()        
+          print("I said yes or no! Try again!")
+          game()   
+   
+##
+##    #question 2
+##    condition = input("Are you nice? y or n ") 
+##    if condition.lower() == "y":
+##        print("Hufflepuff!")
+##    if condition.lower() == "n": 
+##        print("Slytherin")
+game()
+      
